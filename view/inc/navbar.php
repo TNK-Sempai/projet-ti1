c0bddc736303556e26f032c4d594b7e70423de4d
TNK-Sempaï <alioussalah.meidhy@gmail.com>
2026-03-31 15:20:45 +0200

    skills add

--- view/inc/navbar.php
+++ view/inc/navbar.php
@@ -4,7 +4,7 @@
             <ul class="nav-links">
                 <li><a href="./" class="active">Accueil</a></li>
                 <li><a href="?p=about">À propos</a></li>
-                <li><a href="?p=competences">Compétences</a></li>
+                <li><a href="?p=skills">Compétences</a></li>
                 <li><a href="?p=projets">Projets</a></li>
                 <li><a href="?p=contact">Contact</a></li>
             </ul>
